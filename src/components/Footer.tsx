d40c455cfdd72671c77bb0be8e0ccfa9ecb7bca1
JoleyLeung <joley.leung@gmail.com>
2024-10-06 00:27:48 -0400

    fixed image path

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,13 @@
 import React from "react";
 import "../styles/Footer.css";
+import WINSlogo from "../assets/WINSlogo.jpg";
 
 const Footer = () => {
   return (
     <div className="container-fluid footer">
       <div className="row no-gutters">
         <div className="col d-flex align-items-center col-md-auto">
-          <img
-            src="src/assets/WINSlogo.jpg"
-            width={50}
-            height={50}
-            alt="WINS logo"
-          />
+          <img src={WINSlogo} width={50} height={50} alt="WINS logo" />
         </div>
         <div
           className="col d-flex align-items-center"
