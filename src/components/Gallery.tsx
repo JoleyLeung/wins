ccf1cbfc8b5880766b6c4d7a5fa0bee69d7aeb6a
teaxkat <21cats12@gmail.com>
2024-12-02 17:13:06 -0500

    gallery

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Gallery.css";
+import "../App.css";
+import galleryImages from "../../src/assets/gallery_images/Gallery.json";
 
-function Gallery() {
+type Image = {
+  picname: string;
+  pic: string;
+};
+const Gallery: React.FC = () => {
+  // State to hold the images with the correct type
+  const [images, setImages] = useState<Image[]>([]);
+
+  // useEffect to set the images when the component mounts
+  useEffect(() => {
+    setImages(galleryImages);
+  }, []);
+
   return (
-    <div className="gallerya">
-      <p>gallery placeholder =＾● ⋏ ●＾=</p>
+    <div>
+      <div className="d-flex justify-content-center">
+        <span className="bigheader">Gallery</span>
+      </div>
+      <div className="gallery-container">
+        {images.map((image) => (
+          <div key={image.picname} className="image-container">
+            <img src={image.pic} alt={image.picname} />
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default Gallery;
